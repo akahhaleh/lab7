b5084404b00873a17b88237399e372b1095bb4ea
Amr Kahhaleh <akahhaleh@ucsd.edu>
2016-02-17 06:06:03 +0000

    initial analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,14 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(projectLiked);
+}
+
+function projectLiked(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#0000ff");
+    ga("send", "event", 'like', 'click');
 }
